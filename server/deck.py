c4dab1239c28b29a2daae8a9af97c3ed27fac4a6
Damon Doucet <doucet.damon@gmail.com>
2014-11-16 11:51:13 -0800

    Starting deck now has the proper number of each card

--- server/deck.py
+++ server/deck.py
@@ -1,5 +1,5 @@
 from random import shuffle
-from card_decoder.decoder import CardDecoder
+from card_decoder.decoder import CardDecoder, read_card_counts
 
 class Deck(object):
   def __init__(self, cards):
@@ -8,8 +8,11 @@ class Deck(object):
 
   @classmethod
   def create_center_deck(cls, cards):
-    return cls(cards)
-    # TODO(ddoucet): use proper counts of each card
+    card_counts = read_card_counts()
+    deck = []
+    for card in cards:
+      deck.extend([card] * card_counts[card.name])
+    return cls(deck)
 
   def get_next_card(self):
     return self.cards.pop()
