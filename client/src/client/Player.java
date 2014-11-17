ba9c744b1d1a646fd9609adf769dc5f0f3875a55
Rumen Hristov <rhristov@mit.edu>
2014-11-17 00:43:09 -0500

    Add comment

--- client/src/client/Player.java
+++ client/src/client/Player.java
@@ -7,6 +7,12 @@ import com.firebase.client.*;
 
 
 public class Player {
+	// We listen to all of these variables and on every change we update the entry
+	// We will have an instance of the player class for the two players
+	
+	// Who will update the moves? We may still update them and leave the listener
+	// The other player's moves should be entirely to the listener, while ours 
+	// will be update by both server and client
 	Firebase player_node;
 	List<Card> hand, discard, constructs;
 	List<Move> moves;
