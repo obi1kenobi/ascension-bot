7a61160ce28625b1a1831759afecc934ee6059b4
Rumen Hristov <rhristov@mit.edu>
2014-11-23 22:20:32 -0500

    Basic tests for the estimator

---
+++ src/estimators/test_estimators.py
@@ -0,0 +1,45 @@
+import random
+from estimators import AverageEstimator, WeightedAverageEstimator, LinearFitEstimator
+
+def test_average_estimator():
+    est = AverageEstimator()
+    rand_number = random.randint(-100, 100)
+
+    for i in xrange(100):
+        est.push(rand_number)
+
+    assert est.estimate() == rand_number
+
+def test_weighted_estimator():
+    est = WeightedAverageEstimator(random.random())
+    rand_number = random.randint(-100, 100)
+
+    for i in xrange(100):
+        est.push(rand_number)
+
+    assert est.estimate() == rand_number
+
+def test_linear_fit_estimator():
+    est = LinearFitEstimator()
+    rand_number = random.randint(-100, 100)
+
+    for i in xrange(100):
+        est.push(rand_number)
+
+    assert abs(est.estimate() - rand_number) < 1e-9
+
+
+    est = LinearFitEstimator()
+    est.push(100)
+    est.push(200)
+    assert abs(est.estimate() - 300) < 1e-9
+
+
+
+    est = LinearFitEstimator()
+    steps = random.randint(3, 1000)
+
+    est.push(0)
+    for i in xrange(steps):
+        est.push(i + 1)
+        assert abs(est.estimate() - (i + 2)) < 1e-9
