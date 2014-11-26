efd6fa44e1034808226f23179ed7f85bc9c9e634
Predrag Gruevski <predrag.gruevski@gmail.com>
2014-11-26 03:49:30 -0500

    Added new estimator and new functionality.

--- src/estimators/estimators.py
+++ src/estimators/estimators.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 
 # Estimate = average of all pushed values
 class AverageEstimator(object):
@@ -25,7 +26,7 @@ class WeightedAverageEstimator(object):
     if not self.called:
         self.previous = value
         self.called = True
-    else: 
+    else:
         self.previous = (self.previous * (1-self.coeff)) + (self.coeff * value)
 
     return self.estimate()
@@ -55,3 +56,69 @@ class LinearFitEstimator(object):
   def estimate(self):
     x = len(self.history)
     return (self.m * x) + self.c
+
+  # solves quadratic functions of the form ax^2 + bx + c = 0
+  # returns a pair of real solutions (x1, x2), or None if there are none
+  @staticmethod
+  def _solve_quadratic(a, b, c):
+    print "solving (%f)x^2 + (%f)x + (%f) = 0" % (a, b, c)
+    if a == 0:
+      x = -c/b
+      if x < 0:
+        return None
+      return (x, x)
+    else:
+      discriminant = b*b - 4*a*c
+      if discriminant < 0:
+        return None
+      discriminant = sqrt(discriminant)
+
+      x1 = (-b + discriminant) / (2*a)
+      x2 = (-b - discriminant) / (2*a)
+
+      print "solutions: (%f, %f)" % (x1, x2)
+
+      return (x1, x2)
+
+  def estimate_turns_to_sum(self, s):
+    # finds the number of new push/estimate turns it will take to produce a sum equal to s
+    # assuming pushed values follow the estimated model.
+    if self.m == 0:
+      return None
+
+    h = float(len(self.history))
+    total = float(s) + h*self.c + h*(h-1)*self.m/2
+
+    # total = sum (mx + c) for x = 0 ... t-1
+    # total = m/2 * t * (t-1) + ct
+    # total = m/2 * t^2 + (c - m/2) * t
+
+    solns = LinearFitEstimator._solve_quadratic(self.m/2, (self.c - self.m/2), -total)
+    if solns is None:
+      return None
+
+    (t1, t2) = solns
+
+    # count how many new turns are needed, in addition to the h existing ones
+    t1 -= h
+    t2 -= h
+
+    if t1 < 0 and t2 < 0:
+      return None
+    elif t1 >= 0 and t2 >= 0:
+      return min(t1, t2)
+    elif t1 >= 0:
+      return t1
+    elif t2 >= 0:
+      return t2
+
+class LimitedHorizonLinearFitEstimator(LinearFitEstimator):
+  def __init__(self, horizon):
+    super(LimitedHorizonLinearFitEstimator, self).__init__()
+    self.horizon = horizon
+    assert self.horizon > 0
+
+  def push(self, value):
+    if len(self.history) >= self.horizon:
+      self.history.pop(0)
+    super(LimitedHorizonLinearFitEstimator, self).push(value)
--- src/estimators/test_estimators.py
+++ src/estimators/test_estimators.py
@@ -1,54 +1,71 @@
 import random
-from estimators import AverageEstimator, WeightedAverageEstimator, LinearFitEstimator
+from estimators import AverageEstimator, WeightedAverageEstimator, LinearFitEstimator, LimitedHorizonLinearFitEstimator
 
 def test_average_estimator():
-    est = AverageEstimator()
-    rand_number = random.randint(-100, 100)
+  est = AverageEstimator()
+  rand_number = random.randint(-100, 100)
 
-    for i in xrange(100):
-        est.push(rand_number)
+  for i in xrange(100):
+    est.push(rand_number)
 
-    assert abs(est.estimate() - rand_number) < 1e-9
+  assert abs(est.estimate() - rand_number) < 1e-9
 
 def test_weighted_estimator():
-    est = WeightedAverageEstimator(random.random())
-    rand_number = random.randint(-100, 100)
+  est = WeightedAverageEstimator(random.random())
+  rand_number = random.randint(-100, 100)
 
-    for i in xrange(100):
-        est.push(rand_number)
+  for i in xrange(100):
+    est.push(rand_number)
 
-    assert abs(est.estimate() - rand_number) < 1e-9
+  assert abs(est.estimate() - rand_number) < 1e-9
 
 
-    est = WeightedAverageEstimator(0.2)
-    est.push(5)
-    est.push(10)
+  est = WeightedAverageEstimator(0.2)
+  est.push(5)
+  est.push(10)
 
-    assert abs(est.estimate() - 6) < 1e-9
-    
+  assert abs(est.estimate() - 6) < 1e-9
+
+def linear_fit_estimator_test1(est):
+  rand_number = random.randint(-100, 100)
+
+  for i in xrange(100):
+    est.push(rand_number)
+
+  assert abs(est.estimate() - rand_number) < 1e-9
+
+def linear_fit_estimator_test2(est):
+  est.push(100)
+  est.push(200)
+  assert abs(est.estimate() - 300) < 1e-9
+  assert abs(est.estimate_turns_to_sum(300) - 1) < 1e-9
+  assert abs(est.estimate_turns_to_sum(700) - 2) < 1e-9
+  assert abs(est.estimate_turns_to_sum(1200) - 3) < 1e-9
+
+def linear_fit_estimator_test3(est):
+  steps = random.randint(3, 1000)
+
+  est.push(0)
+  for i in xrange(steps):
+    est.push(i + 1)
+    assert abs(est.estimate() - (i + 2)) < 1e-9
 
 def test_linear_fit_estimator():
-    est = LinearFitEstimator()
-    rand_number = random.randint(-100, 100)
+  est = LinearFitEstimator()
+  linear_fit_estimator_test1(est)
 
-    for i in xrange(100):
-        est.push(rand_number)
+  est = LinearFitEstimator()
+  linear_fit_estimator_test2(est)
 
-    assert abs(est.estimate() - rand_number) < 1e-9
+  est = LinearFitEstimator()
+  linear_fit_estimator_test3(est)
 
+def test_limited_horizon_linear_fit_estimator():
+  est = LimitedHorizonLinearFitEstimator(5)
+  linear_fit_estimator_test1(est)
 
+  est = LimitedHorizonLinearFitEstimator(5)
+  linear_fit_estimator_test2(est)
 
-    est = LinearFitEstimator()
-    est.push(100)
-    est.push(200)
-    assert abs(est.estimate() - 300) < 1e-9
-
-
-
-    est = LinearFitEstimator()
-    steps = random.randint(3, 1000)
-
-    est.push(0)
-    for i in xrange(steps):
-        est.push(i + 1)
-        assert abs(est.estimate() - (i + 2)) < 1e-9
+  est = LimitedHorizonLinearFitEstimator(5)
+  linear_fit_estimator_test3(est)
